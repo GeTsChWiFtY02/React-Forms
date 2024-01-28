f23c5046b276a7dad23abe77f3aa7a518c498475
GeTsChWiFtY02 <141801363+GeTsChWiFtY02@users.noreply.github.com>
2024-01-27 21:05:21 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { useState } from 'react'
+import SignUpForm from './components/SignUpForm'
+import Authenticate from './components/Authenticate'
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  return (
+    <>
+      <h1>React Forms<br/>
+      Block 27 Workshop</h1>
+
+      <div className="myFormContainer">
+        <SignUpForm token={token} setToken={setToken} />
+        <br />
+        <hr />
+        <Authenticate token={token} setToken={setToken}/>
+      </div>
+    </>
+  )
+}
+
+export default App
